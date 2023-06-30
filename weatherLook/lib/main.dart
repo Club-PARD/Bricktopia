3402447e44a9d2d79303149e963ed6ad96f3987f
정성국 <ssoup0224@gmail.com>
2023-06-30 09:11:10 +0900

    Update main.dart

--- weatherLook/lib/main.dart
+++ weatherLook/lib/main.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:homepage/home/homepage.dart';
-
-import 'chatbot/constants/themes.dart';
+import 'package:homepage/app.dart';
 
 void main() {
-  runApp(const ProviderScope(child: App()));
-}
-
-class App extends ConsumerWidget {
-  const App({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    //final activeTheme = ref.watch(activeThemeProvider);
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: mainTheme,
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: WeatherLook()));
 }
