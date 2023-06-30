731e5ceef6a5a7a77b3d9124073bcb028df236ee
정성국 <ssoup0224@gmail.com>
2023-06-30 09:11:21 +0900

    Update fivedayWeather.dart

--- weatherLook/lib/chatbot/widgets/fivedayWeather.dart
+++ weatherLook/lib/chatbot/widgets/fivedayWeather.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names, avoid_print
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -44,7 +46,7 @@ class _FivedayWeatherState extends State<FivedayWeather> {
           minTemperature: minTemp.toDouble(),
           humidity: humidity.toDouble(),
           main: main.toString(),
-          pop : pop.toDouble(),
+          pop: pop.toDouble(),
         );
         dataList.add(weatherData);
       }
@@ -56,7 +58,8 @@ class _FivedayWeatherState extends State<FivedayWeather> {
     }
   }
 
-  List<List<WeatherData>> groupWeatherDataByDate(List<WeatherData> weatherDataList) {
+  List<List<WeatherData>> groupWeatherDataByDate(
+      List<WeatherData> weatherDataList) {
     final groupedData = <List<WeatherData>>[];
     for (final weatherData in weatherDataList) {
       bool foundGroup = false;
@@ -75,10 +78,13 @@ class _FivedayWeatherState extends State<FivedayWeather> {
   }
 
   bool isSameDate(DateTime date1, DateTime date2) {
-    return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day;
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
   }
 
-  List<WeatherData> compareWeatherData(List<List<WeatherData>> groupedDataList) {
+  List<WeatherData> compareWeatherData(
+      List<List<WeatherData>> groupedDataList) {
     final List<WeatherData> comparedList = [];
     for (final group in groupedDataList) {
       double maxTemperature = group.first.maxTemperature;
