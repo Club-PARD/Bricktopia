e1558ae3c0fb2f67c9fcbfa00987e809d844e8e3
s1mjane <s1mjane@handong.ac.kr>
2023-07-03 15:19:06 +0900

    janee 2

--- koo_weatherlook/lib/home/createFeedback.dart
+++ koo_weatherlook/lib/home/createFeedback.dart
@@ -10,6 +10,8 @@ class createFeedbackPage extends StatefulWidget {
 }
 
 class _createFeedbackPageState extends State<createFeedbackPage> {
+  bool isToggleOn = false; // 초기 토글 상태
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,32 +61,211 @@ class _createFeedbackPageState extends State<createFeedbackPage> {
                       '오늘 추천해드린 옷차림 아이템들에\n대해서 만족하셨나요?',
                       style: TextStyle(fontSize: 18),
                     ),
-                    SizedBox(
-                        height: (MediaQuery.of(context).size.height) / 33.33),
-
+                    SizedBox(height: (MediaQuery.of(context).size.height) / 45),
                     const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         CircleButton(),
                       ],
                     ),
-
-                    SizedBox(
-                        height: (MediaQuery.of(context).size.height) / 16.6),
+                    SizedBox(height: (MediaQuery.of(context).size.height) / 25),
                     const Text(
                       '불필요한 아이템이 있으셨나요?',
                       style: TextStyle(fontSize: 18),
                     ),
-                    // 박스
-                    SizedBox(
-                        height: (MediaQuery.of(context).size.height) / 20.5),
+                    SizedBox(height: (MediaQuery.of(context).size.height) / 45),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(
+                        ((MediaQuery.of(context).size.width) / 23),
+                        ((MediaQuery.of(context).size.height) / 67),
+                        ((MediaQuery.of(context).size.width) / 23),
+                        ((MediaQuery.of(context).size.height) / 67),
+                      ),
+                      width: (MediaQuery.of(context).size.width) / 1.22,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.77), // 배경색 지정
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          color: const Color(
+                              0xff4E5FFF), // Set the border color here
+                          width: 2.0, // Set the border thickness here
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Column(children: [
+                            Center(
+                                // 상의
+                                child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Image.asset('assets/items/paddedCoat.png',
+                                    width:
+                                        (MediaQuery.of(context).size.height) /
+                                            10),
+                                SizedBox(
+                                    width: (MediaQuery.of(context).size.width) /
+                                        18.95),
+                                Image.asset('assets/items/hoodie.png',
+                                    width:
+                                        (MediaQuery.of(context).size.height) /
+                                            10),
+                                SizedBox(
+                                    width: (MediaQuery.of(context).size.width) /
+                                        18.95),
+                                Image.asset('assets/items/shorts.png',
+                                    width:
+                                        (MediaQuery.of(context).size.height) /
+                                            10),
+                              ],
+                            )),
+                            SizedBox(
+                                height:
+                                    (MediaQuery.of(context).size.height) / 45),
+                            Center(
+                                child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Image.asset('assets/items/sneakers.png',
+                                    width:
+                                        (MediaQuery.of(context).size.height) /
+                                            10),
+                                SizedBox(
+                                    width: (MediaQuery.of(context).size.width) /
+                                        18.95),
+                                Image.asset('assets/items/cap.png',
+                                    width:
+                                        (MediaQuery.of(context).size.height) /
+                                            10),
+                                // SizedBox(
+                                //     width: (MediaQuery.of(context).size.width) /
+                                //         18.95),
+                                // Image.asset('assets/items/shorts.png',
+                                //     width:
+                                //         (MediaQuery.of(context).size.height) /
+                                //             10),
+                              ],
+                            )),
+                          ])
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: (MediaQuery.of(context).size.height) / 25),
                     const Text(
                       '더 필요한 아이템이 있으셨나요?(선택사항)',
                       style: TextStyle(fontSize: 18),
                     ),
-                    SizedBox(height: (MediaQuery.of(context).size.height) / 32),
-                    // 텍스트필드
-                    SizedBox(height: (MediaQuery.of(context).size.height) / 22),
+                    SizedBox(height: (MediaQuery.of(context).size.height) / 60),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isToggleOn = false; // 예 선택
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            elevation: 0,
+                            backgroundColor: isToggleOn
+                                ? const Color(0xff4E5FFF)
+                                : const Color(0xffE4E8F6),
+                            minimumSize: const Size(50, 38), // 버튼의 최소 높이 설정
+                          ),
+                          child: const Text(
+                            '상의',
+                            style: TextStyle(color: Colors.black, fontSize: 12),
+                          ),
+                        ),
+                        SizedBox(
+                            width: (MediaQuery.of(context).size.width) /
+                                35), // 간격 조정
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isToggleOn = false; // 아니오 선택
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            elevation: 0,
+                            backgroundColor: isToggleOn
+                                ? const Color(0xff4E5FFF)
+                                : const Color(0xffE4E8F6),
+                            minimumSize: const Size(50, 38), // 버튼의 최소 높이 설정
+                          ),
+                          child: const Text(
+                            '하의',
+                            style: TextStyle(color: Colors.black, fontSize: 12),
+                          ),
+                        ),
+                        SizedBox(
+                            width: (MediaQuery.of(context).size.width) /
+                                35), // 간격 조정
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isToggleOn = false; // 아니오 선택
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            elevation: 0,
+                            backgroundColor: isToggleOn
+                                ? const Color(0xff4E5FFF)
+                                : const Color(0xffE4E8F6),
+                            minimumSize: const Size(50, 38), // 버튼의 최소 높이 설정
+                          ),
+                          child: const Text(
+                            '신발',
+                            style: TextStyle(color: Colors.black, fontSize: 12),
+                          ),
+                        ),
+                        SizedBox(
+                            width: (MediaQuery.of(context).size.width) /
+                                35), // 간격 조정
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isToggleOn = false; // 아니오 선택
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            elevation: 0,
+                            backgroundColor: isToggleOn
+                                ? const Color(0xff4E5FFF)
+                                : const Color(0xffE4E8F6),
+                            minimumSize: const Size(80, 38), // 버튼의 최소 높이 설정
+                          ),
+                          child: const Text(
+                            '악세사리',
+                            style: TextStyle(color: Colors.black, fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: (MediaQuery.of(context).size.height) / 80),
+                    SizedBox(
+                      height: 50,
+                      width: (MediaQuery.of(context).size.width) / 1.24,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintText: '세부 아이템', // 힌트 텍스트 설정
+                          hintStyle:
+                              TextStyle(color: Color(0xff898989), fontSize: 14),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(0xff4E5FFF)), // 밑줄의 색상을 보라색으로 설정
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: (MediaQuery.of(context).size.height) / 23),
                   ],
                 ),
               ],
@@ -142,27 +323,49 @@ class _CircleButtonState extends State<CircleButton> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: List.generate(2, (index) {
-        return GestureDetector(
-          onTap: () {
-            setState(() {
-              selectedValue = index + 1;
-            });
-          },
-          child: Container(
-            width: 24.0,
-            height: 24.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: selectedValue == index + 1 ? Colors.blue : Colors.grey,
+        return Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedValue = index + 1;
+                });
+              },
+              child: Container(
+                width: 24.0,
+                height: 24.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: selectedValue == index + 1
+                        ? const Color(0xff4E5FFF)
+                        : const Color(0xff4E5FFF),
+                    width: 2.0,
+                  ),
+                  color: selectedValue == index + 1
+                      ? const Color(0xff4E5FFF)
+                      : const Color(0xffffffff),
+                ),
+              ),
             ),
-          ),
+            SizedBox(
+                width: (MediaQuery.of(context).size.width) /
+                    21), // 토글 버튼과 텍스트 사이의 간격 조정
+            Text(
+              index == 0 ? '예' : '아니오',
+              style: TextStyle(
+                fontSize: 20.0, // 텍스트 크기를 20으로 설정
+                color: selectedValue == index + 1
+                    ? const Color(0xff000000)
+                    : const Color(0xff000000),
+              ),
+            ),
+            SizedBox(
+                width: (MediaQuery.of(context).size.width) /
+                    5), // 토글 버튼과 텍스트 사이의 간격을 8.0으로 조정
+          ],
         );
-      })
-          .expand((button) => [
-                button,
-                const SizedBox(width: 16.0), // 버튼 사이의 간격 조정
-              ])
-          .toList(),
+      }),
     );
   }
 }
