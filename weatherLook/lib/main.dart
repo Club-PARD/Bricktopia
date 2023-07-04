adaa9acf01ef30d7bf5a823ceff3194f81c3d661
정성국 <ssoup0224@gmail.com>
2023-07-05 03:35:55 +0900

    Update main.dart

--- weatherLook/lib/main.dart
+++ weatherLook/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:homepage/app.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const ProviderScope(child: App()));
+  initializeDateFormatting('ko_KR', null).then((_) {
+    runApp(const ProviderScope(child: App()));
+  });
 }
