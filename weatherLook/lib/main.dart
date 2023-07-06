6834c7a9ac99fc464f18c8c5ecf38040c0e6bd7e
정성국 <ssoup0224@gmail.com>
2023-07-07 02:22:47 +0900

    main provider

--- weatherLook/lib/main.dart
+++ weatherLook/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:weather_summary/app.dart';
 
@@ -7,6 +8,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   initializeDateFormatting('ko_KR', null).then((_) {
-    runApp(const MyApp());
+    runApp(const ProviderScope(child: MyApp()));
   });
 }
